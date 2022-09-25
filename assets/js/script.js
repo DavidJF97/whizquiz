173599a658715a2ceda992df34c325bb108a08fa
David J-F <davidjf97@yahoo.ie>
2022-09-25 17:56:49 +0000

    Add change index to game page, add new index page and highscore page

---
+++ assets/js/script.js
@@ -0,0 +1,85 @@
+const start = document.getElementbyId("start");
+const quiz =  document.getElementbyId("quiz");
+const question = document.getElementbyId("question");
+const questionPic = document.getElementbyId("question-pic");
+const optionA = document.getElementbyId("A");
+const optionB = document.getElementbyId("B");
+const optionC = document.getElementbyId("C");
+const progress = document.getElementbyId("progress");
+const score = document.getElementbyId("score");
+
+/** All the questions*/
+
+let questions = [
+    {
+        question: "1",
+        imgSrc: "",
+        optionA: "",
+        optionB: "",
+        optionC: "",
+        correct: ""
+    }, {
+        question: "2",
+        imgSrc: "",
+        optionA: "",
+        optionB: "",
+        optionC: "",
+        correct: ""
+    }, {
+        question: "3",
+        imgSrc: "",
+        optionA: "",
+        optionB: "",
+        optionC: "",
+        correct: ""
+    }, {
+        question: "4",
+        imgSrc: "",
+        optionA: "",
+        optionB: "",
+        optionC: "",
+        correct: ""
+    }, {
+        question: "5",
+        imgSrc: "",
+        optionA: "",
+        optionB: "",
+        optionC: "",
+        correct: ""
+    }, {
+        question: "6",
+        imgSrc: "",
+        optionA: "",
+        optionB: "",
+        optionC: "",
+        correct: ""
+    }, {
+        question: "7",
+        imgSrc: "",
+        optionA: "",
+        optionB: "",
+        optionC: "",
+        correct: ""
+    }, {
+        question: "8",
+        imgSrc: "",
+        optionA: "",
+        optionB: "",
+        optionC: "",
+        correct: ""
+    }, {
+        question: "9",
+        imgSrc: "",
+        optionA: "",
+        optionB: "",
+        optionC: "",
+        correct: ""
+    }, {
+        question: "10",
+        imgSrc: "",
+        optionA: "",
+        optionB: "",
+        optionC: "",
+        correct: ""
+    }
+];
